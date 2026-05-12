096dacca76874c4bdfa661b43b2f466e0669c34b
Quattro <jesse.quattrociocchi@parsons.us>
2026-05-12 16:44:41 -0500

    Docker updates

--- docker/build_docker.sh
+++ docker/build_docker.sh
@@ -1,1 +1,18 @@
-docker build -t bpp-training-pipeline:cuda12.8 .
+#!/usr/bin/env bash
+set -euo pipefail
+
+# Run from within bev-patch-pf/docker
+
+IMAGE_NAME="${IMAGE_NAME:-bpp-training-pipeline:cuda12.8}"
+BEV_PATCH_PF_COMMIT="${BEV_PATCH_PF_COMMIT:-quattro}"
+
+USER_UID="$(id -u)"
+USER_GID="$(id -g)"
+
+docker build \
+    --build-arg USER_UID="${USER_UID}" \
+    --build-arg USER_GID="${USER_GID}" \
+    --build-arg BEV_PATCH_PF_COMMIT="${BEV_PATCH_PF_COMMIT}" \
+    -t "${IMAGE_NAME}" \
+    -f Dockerfile \
+    .
--- docker/run_docker.sh
+++ docker/run_docker.sh
@@ -1,21 +1,64 @@
 #!/usr/bin/env bash
-set -e
+set -euo pipefail
 
-IMAGE_NAME="bpp-training-pipeline:cuda12.8"
+IMAGE_NAME="${IMAGE_NAME:-bpp-training-pipeline:cuda12.8}"
 
-if [ $# -lt 1 ]; then
-  echo "Usage: $0 /path/to/host/data-dir"
+usage() {
+  echo "Usage:"
+  echo "  $0 /path/to/your/data"
+  echo
+  echo "Example:"
+  echo "  $0 \$HOME/bev-patch-pf/data"
+  echo
+  echo "The provided host data directory will be mounted to:"
+  echo "  /workspace/bev-patch-pf/data"
+  echo
+}
+
+if [ $# -ne 1 ]; then
+  echo "Error: you must provide one argument that is the host data directory"
+  echo
+  usage
   exit 1
 fi
 
 HOST_DATA_DIR="$(realpath "$1")"
 
 if [ ! -d "$HOST_DATA_DIR" ]; then
-  echo "Error: data directory does not exist: $HOST_DATA_DIR"
+  echo "Error: data directory does not exist:"
+  echo " ${HOST_DATA_DIR}"
+  echo
+  echo "Create it first"
+  echo
+  usage
   exit 1
 fi
 
-docker run --rm -it --gpus all \
-  -v "$HOST_DATA_DIR":/workspace/bev-patch-pf/data \
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+# paths relative to bev-patch-pf/docker
+HOST_CONFIG_DIR="${SCRIPT_DIR}/../config"
+HOST_OUTPUT_DIR="${SCRIPT_DIR}/../output"
+HOST_DATASET_DIR="${SCRIPT_DIR}/../src/dataset"
+HOST_WANDB_DIR="${SCRIPT_DIR}/../wandb"
+
+xhost +local:docker
+# Make sure xhost privilege gets revoked
+cleanup() {
+  xhost -local:docker >/dev/null 2>&1 || true
+}
+trap cleanup EXIT
+
+# --ipc=host added to give container all shared memory since training uses a lot of memory
+# Add --net=host if problems with wandb
+docker run -it --rm --gpus all \
+  --ipc=host \
+  -e DISPLAY="${DISPLAY}" \
+  -e QT_X11_NO_MITSHM=1 \
+  -v /tmp/.X11-unix:/tmp/.X11-unix:rw \
+  -v "${HOST_DATA_DIR}:/workspace/bev-patch-pf/data:rw" \
+  -v "${HOST_CONFIG_DIR}:/workspace/bev-patch-pf/config:rw" \
+  -v "${HOST_OUTPUT_DIR}:/workspace/bev-patch-pf/output:rw" \
+  -v "${HOST_DATASET_DIR}:/workspace/bev-patch-pf/src/dataset:rw" \
+  -v "${HOST_WANDB_DIR}:/workspace/bev-patch-pf/wandb:rw" \
   -w /workspace/bev-patch-pf \
-  "$IMAGE_NAME"
+  "${IMAGE_NAME}"
